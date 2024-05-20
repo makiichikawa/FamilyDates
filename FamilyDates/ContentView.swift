4a61b254092cd73853883d010656b2efc73a55f5
makiichikawa <punkfolkfolk@gmail.com>
2024-05-20 22:51:59 +0900

    Initial Commit

---
+++ FamilyDates/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FamilyDates
+//
+//  Created by 市川真希 on 2024/05/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FamilyDates/FamilyDatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyDatesApp.swift
+//  FamilyDates
+//
+//  Created by 市川真希 on 2024/05/20.
+//
+
+import SwiftUI
+
+@main
+struct FamilyDatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
