60026a0d0573119d8ee50e4bda89fa9fbdc4ce86
makiichikawa <punkfolkfolk@gmail.com>
2024-05-21 23:02:07 +0900

    一覧画面のビューのみを作成

--- FamilyDates/ContentView.swift
+++ FamilyDates/ContentView.swift
@@ -10,12 +10,40 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Navigation bar
+            HStack {
+                Image(systemName: "line.horizontal.3")
+                Spacer()
+                Text("Family Dates").font(.headline)
+                Spacer()
+                Image(systemName: "house")
+                Image(systemName: "ellipsis")
+            }.padding().background(Color(UIColor.systemGray6))
+            // Table view
+            List {
+                HStack {
+                    Text("名前").frame(width: 100, alignment: .leading)
+                    Spacer()
+                    Text("年齢").frame(width: 50, alignment: .center)
+                    Spacer()
+                    Text("記念日").frame(width: 100, alignment: .leading)
+                }.font(.headline)
+                HStack {
+                    Text("自分").frame(width: 100, alignment: .leading)
+                    Spacer()
+                    Text("36").frame(width: 50, alignment: .center)
+                    Spacer()
+                    Text("-").frame(width: 100, alignment: .leading)
+                }
+                HStack {
+                    Text("母").frame(width: 100, alignment: .leading)
+                    Spacer()
+                    Text("59").frame(width: 50, alignment: .center)
+                    Spacer()
+                    Text("還暦祝い").frame(width: 100, alignment: .leading)
+                }
+            }
         }
-        .padding()
     }
 }
 
