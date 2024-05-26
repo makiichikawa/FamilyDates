836bd9e69b111bd90e1414a7d27e257fee2e96a8
makiichikawa <punkfolkfolk@gmail.com>
2024-05-26 20:36:45 +0900

    一覧から詳細に遷移できるように変更

--- FamilyDates/ContentView.swift
+++ FamilyDates/ContentView.swift
@@ -10,22 +10,26 @@ import SwiftUI
 struct ContentView: View {
     @ObservedObject var Family = People()
     var body: some View {
-        VStack {
-            NavigationBar()
-            // Table view
-            List {
-                HStack {
-                    Text("名前").frame(width: 100, alignment: .leading)
-                    Spacer()
-                    Text("年齢").frame(width: 50, alignment: .center)
-                    Spacer()
-                    Text("記念日").frame(width: 100, alignment: .leading)
-                }.font(.headline)
-                ForEach(Family.people) { person in
-                    ListRow(name: person.name, age: person.age, event: person.event)
+        NavigationView {
+            VStack {
+                NavigationBar()
+                // Table view
+                List {
+                    HStack {
+                        Text("名前").frame(width: 100, alignment: .leading)
+                        Spacer()
+                        Text("年齢").frame(width: 50, alignment: .center)
+                        Spacer()
+                        Text("記念日").frame(width: 100, alignment: .leading)
+                    }.font(.headline)
+                    ForEach(Family.people) { person in
+                        NavigationLink(destination: ShowView(human: person)){
+                            ListRow(name: person.name, age: person.age, event: person.event)
+                        }
+                    }
                 }
-            }
-        }.background(Color(UIColor.systemGray6))
+            }.background(Color(UIColor.systemGray6))
+        }
     }
 }
 
